612836cdba87da50c240eca0aab94e42d8cf8ec8
Cyber MacGeddon <cybermaggedon@gmail.com>
2025-08-11 22:29:49 +0100

    Tests reworked for OVHcloud

---
+++ pulumi/jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/__tests__'],
+  testMatch: ['**/*.test.ts'],
+  setupFilesAfterEnv: ['<rootDir>/__tests__/setup.ts'],
+  collectCoverageFrom: [
+    '**/*.ts',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/__tests__/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        // TypeScript options for tests
+        target: 'es2018',
+        module: 'commonjs',
+        lib: ['es2018'],
+        strict: true,
+        esModuleInterop: true,
+        skipLibCheck: true,
+        forceConsistentCasingInFileNames: true,
+      },
+    },
+  },
+};
